31cbf5bd7fa7c5e3b92f6678ceae473e7def067b
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-30 14:30:18 +0900

    test8까지 클론

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -1,11 +1,19 @@
 package com.ll.exam.qsl.user.repository;
 
-import com.ll.exam.qsl.user.entity.QSiteUser;
 import com.ll.exam.qsl.user.entity.SiteUser;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
+import java.util.function.LongSupplier;
 
 import static com.ll.exam.qsl.user.entity.QSiteUser.siteUser;
 
@@ -25,5 +33,74 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                 .from(siteUser)
                 .where(siteUser.id.eq(id))
                 .fetchOne();
+    }
+
+    @Override
+    public long getQslCount() {
+        return jpaQueryFactory
+                .select(siteUser.count())
+                .from(siteUser)
+                .fetchOne();
+    }
+
+    @Override
+    public SiteUser getQslUserOrderByIdAscOne() {
+        return jpaQueryFactory
+                .select(siteUser)
+                .from(siteUser)
+                .orderBy(siteUser.id.asc())
+                .limit(1)
+                .fetchFirst();
+    }
+
+    @Override
+    public List<SiteUser> getQslUsersOrderByIdAsc() {
+        return jpaQueryFactory
+                .select(siteUser)
+                .from(siteUser)
+                .orderBy(siteUser.id.asc())
+                .fetch();
+    }
+
+    @Override
+    public List<SiteUser> searchQsl(String kw) {
+        return jpaQueryFactory
+                .select(siteUser)
+                .from(siteUser)
+                .where(
+                        siteUser.username.contains(kw)
+                                .or(siteUser.email.contains(kw))
+                )
+                .orderBy(siteUser.id.desc())
+                .fetch();
+    }
+
+    @Override
+    public Page<SiteUser> searchQsl(String kw, Pageable pageable) {
+        JPAQuery<SiteUser> usersQuery = jpaQueryFactory
+                .select(siteUser)
+                .from(siteUser)
+                .where(
+                        siteUser.username.contains(kw)
+                                .or(siteUser.email.contains(kw))
+                )
+                .offset(pageable.getOffset()) // 몇개를 건너 띄어야 하는지 LIMIT {1}, ?
+                .limit(pageable.getPageSize()); // 한페이지에 몇개가 보여야 하는지 LIMIT ?, {1}
+        for (Sort.Order o : pageable.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(siteUser.getType(), siteUser.getMetadata());
+            usersQuery.orderBy(new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(o.getProperty())));
+        }
+
+        List<SiteUser> users = usersQuery.fetch();
+
+        JPAQuery<Long> usersCountQuery = jpaQueryFactory
+                .select(siteUser.count())
+                .from(siteUser)
+                .where(
+                        siteUser.username.contains(kw)
+                                .or(siteUser.email.contains(kw))
+                );
+
+        return PageableExecutionUtils.getPage(users, pageable, usersCountQuery::fetchOne);
     }
 }
--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -6,13 +6,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+// 이렇게 클래스 @Transactional를 붙이면, 클래스의 각 테스트케이스에 전부 @Transactional 붙은 것과 동일
+// @Test + @Transactional 조합은 자동으로 롤백을 유발시킨다.
+@Transactional
+@ActiveProfiles("test") // 테스트 모드 활성화
 class UserRepositoryTests {
     @Autowired
     private UserRepository userRepository;
@@ -20,18 +31,23 @@ class UserRepositoryTests {
     @Test
     @DisplayName("회원 생성")
     void t1() {
-        SiteUser u1 = SiteUser.builder()
-                .username("user1")
+        SiteUser u3 = SiteUser.builder()
+                .username("user3")
                 .password("{noop}1234")
-                .email("user1@test.com")
+                .email("user3@test.com")
                 .build();
-        SiteUser u2 = new SiteUser(null, "user2", "{noop}1234", "user2@test.com");
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        SiteUser u4 = SiteUser.builder()
+                .username("user4")
+                .password("{noop}1234")
+                .email("user4@test.com")
+                .build();
+
+        userRepository.saveAll(Arrays.asList(u3, u4));
     }
 
     @Test
-    @DisplayName("1번 회원 찾기")
+    @DisplayName("1번 회원을 Qsl로 가져오기")
     void t2() {
         SiteUser u1 = userRepository.getQslUser(1L);
 
@@ -39,5 +55,136 @@ class UserRepositoryTests {
         assertThat(u1.getUsername()).isEqualTo("user1");
         assertThat(u1.getPassword()).isEqualTo("{noop}1234");
         assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+    }
+
+    @Test
+    @DisplayName("2번 회원을 Qsl로 가져오기")
+    void t3() {
+        SiteUser u2 = userRepository.getQslUser(2L);
+
+        assertThat(u2.getId()).isEqualTo(2L);
+        assertThat(u2.getUsername()).isEqualTo("user2");
+        assertThat(u2.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u2.getPassword()).isEqualTo("{noop}1234");
+    }
+
+    @Test
+    @DisplayName("모든 회원 수")
+    void t4() {
+        long count = userRepository.getQslCount();
+
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("가장 오래된 회원 1명")
+    void t5() {
+        SiteUser u1 = userRepository.getQslUserOrderByIdAscOne();
+
+        assertThat(u1.getId()).isEqualTo(1L);
+        assertThat(u1.getUsername()).isEqualTo("user1");
+        assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
+    }
+
+    @Test
+    @DisplayName("전체회원, 오래된 순")
+    void t6() {
+        List<SiteUser> users = userRepository.getQslUsersOrderByIdAsc();
+
+        SiteUser u1 = users.get(0);
+
+        assertThat(u1.getId()).isEqualTo(1L);
+        assertThat(u1.getUsername()).isEqualTo("user1");
+        assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
+
+        SiteUser u2 = users.get(1);
+
+        assertThat(u2.getId()).isEqualTo(2L);
+        assertThat(u2.getUsername()).isEqualTo("user2");
+        assertThat(u2.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u2.getPassword()).isEqualTo("{noop}1234");
+    }
+
+    @Test
+    @DisplayName("검색, List 리턴")
+    void t7() {
+        // 검색대상 : username, email
+        // user1 로 검색
+        List<SiteUser> users = userRepository.searchQsl("user1");
+
+        assertThat(users.size()).isEqualTo(1);
+
+        SiteUser u = users.get(0);
+
+        assertThat(u.getId()).isEqualTo(1L);
+        assertThat(u.getUsername()).isEqualTo("user1");
+        assertThat(u.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u.getPassword()).isEqualTo("{noop}1234");
+
+        // user2 로 검색
+        users = userRepository.searchQsl("user2");
+
+        assertThat(users.size()).isEqualTo(1);
+
+        u = users.get(0);
+
+        assertThat(u.getId()).isEqualTo(2L);
+        assertThat(u.getUsername()).isEqualTo("user2");
+        assertThat(u.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u.getPassword()).isEqualTo("{noop}1234");
+    }
+
+    @Test
+    @DisplayName("검색, Page 리턴, id DESC, pageSize=1, page=0")
+    void t8() {
+        long totalCount = userRepository.count();
+        int pageSize = 1; // 한 페이지에 보여줄 아이템 개수
+        int totalPages = (int)Math.ceil(totalCount / (double)pageSize);
+        int page = 1;
+        String kw = "user";
+
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.asc("id"));
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(sorts)); // 한 페이지에 10까지 가능
+        Page<SiteUser> usersPage = userRepository.searchQsl(kw, pageable);
+
+        assertThat(usersPage.getTotalPages()).isEqualTo(totalPages);
+        assertThat(usersPage.getNumber()).isEqualTo(page);
+        assertThat(usersPage.getSize()).isEqualTo(pageSize);
+
+        List<SiteUser> users = usersPage.get().toList();
+
+        assertThat(users.size()).isEqualTo(pageSize);
+
+        SiteUser u = users.get(0);
+
+        assertThat(u.getId()).isEqualTo(2L);
+        assertThat(u.getUsername()).isEqualTo("user2");
+        assertThat(u.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u.getPassword()).isEqualTo("{noop}1234");
+        // 검색어 : user1
+        // 한 페이지에 나올 수 있는 아이템 수 : 1개
+        // 현재 페이지 : 1
+        // 정렬 : id 역순
+
+        // 내용 가져오는 SQL
+        /*
+        SELECT site_user.*
+        FROM site_user
+        WHERE site_user.username LIKE '%user%'
+        OR site_user.email LIKE '%user%'
+        ORDER BY site_user.id ASC
+        LIMIT 1, 1
+         */
+
+        // 전체 개수 계산하는 SQL
+        /*
+        SELECT COUNT(*)
+        FROM site_user
+        WHERE site_user.username LIKE '%user%'
+        OR site_user.email LIKE '%user%'
+         */
     }
 }
